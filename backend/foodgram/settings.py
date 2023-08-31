f64019075c08ce0f2a8e3c0fd01b7669b447c6b7
Mikhail <mikhail@sudarkin.com>
2023-09-01 01:34:07 +0500

    Fix.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,11 +23,11 @@ env = dotenv_values()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = env['SECRET_KEY']
-SECRET_KEY = str(os.getenv('SECRET_KEY'))
+SECRET_KEY = env['SECRET_KEY']
+# SECRET_KEY = str(os.getenv('SECRET_KEY'))
 DEBUG = True
-# ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split()
-ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS'))
+ALLOWED_HOSTS = env['ALLOWED_HOSTS'].split()
+# ALLOWED_HOSTS = str(os.getenv('ALLOWED_HOSTS'))
 
 
 
