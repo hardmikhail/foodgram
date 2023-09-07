3b5b284f905ab6d504755e6fa0b116af6e5631e4
Mikhail <mikhail@sudarkin.com>
2023-09-07 12:36:38 +0500

    Fix.

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$ir=#wc$&*pksl&^lenzy&x0d)i5=0@m-dp3q$7v6ee$qtnbfo'
 DEBUG = True
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split()
-ALLOWED_HOSTS = '127.0.0.1 localhost [::1] host.docker.internal fdgram.myvnc.com'
+ALLOWED_HOSTS = '127.0.0.1 localhost [::1] host.docker.internal fdgram.myvnc.com'.split()
 
 
 # Application definition
